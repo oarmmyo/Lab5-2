92540f4bdd3402765483a01f7259ab3e01e7b5b6
Theeranai Nakpradit <9nongarm@gmail.com>
2020-09-27 13:21:04 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int a,x;
+  printf("Enter a :");
+  scanf("%d",&a);
+  if(a > 0)
+    x = a*15;
+  else
+    x = a*(-15);
+  printf("x = %d",x);
+  return 0;
+}
